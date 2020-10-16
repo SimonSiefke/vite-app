193f66812370bdfc6503c2f31e885fb598e9d9ac
Simon Siefke <simon.siefke@gmail.com>
2020-10-16 17:12:57 +0200

    add score

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,77 @@ import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+const KununuScore1 = () => (
+  <a
+    href="https://www.kununu.com/de/enercity?utm_source=widget&utm_campaign=widget_selfservice_scoresmall"
+    target="_blank"
+    rel="nofollow"
+  >
+    <img
+      src="https://www.kununu.com/de/partner/KlVXDFQ%3D/self-service-button?button-type=1"
+      width="240"
+      height="90"
+      alt="kununu"
+    />
+  </a>
+)
+
+const KununuScore2 = () => (
+  <a
+    href="https://www.kununu.com/de/enercity?utm_source=widget&utm_campaign=widget_selfservice_scorelarge"
+    target="_blank"
+    rel="nofollow"
+  >
+    <img
+      src="https://www.kununu.com/de/partner/KlVXDFQ%3D/self-service-button?button-type=3"
+      width="150"
+      height="100"
+      alt="kununu"
+    />
+  </a>
+)
+
+// const KununuScore3 = () => {
+//   const KUNUNU_HASH_ENERCITY = `KlVXDFQ=`
+//   const KUNUNU_IFRAME_URL = `https://www.kununu.com/at/partner/${KUNUNU_HASH_ENERCITY}/v2-overall-large`
+
+//   return (
+//     <iframe
+//       src={KUNUNU_IFRAME_URL}
+//       width={300}
+//       height={300}
+//       style={{ border: 'none' }}
+//     />
+//   )
+// }
+
+const KununuScore3 = () => (
+  <a
+    href="https://www.kununu.com/de/enercity?utm_source=widget&utm_campaign=widget_selfservice_scorelarge"
+    target="_blank"
+    rel="nofollow"
+  >
+    <img
+      src="https://www.kununu.com/de/partner/KlVXDFQ%3D/self-service-button?button-type=3"
+      width="150"
+      height="100"
+      alt="kununu"
+    />
+  </a>
+)
+
 function App() {
   const [count, setCount] = useState(10)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Option 1 <KununuScore1 />
+      <br />
+      <br />
+      Option 2 <KununuScore2 />
+      <br />
+      <br />
+      Option 3 <KununuScore3 />
     </div>
   )
 }
